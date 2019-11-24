97b14549a0f0744319fb4213f3aa659f2cc14ef5
Vincenzo Vigilante <info@vvigilante.com>
2019-11-24 17:54:25 +0100

    version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ clean_dist()
 
 setup(
     name='ytsubtitles',
-    version="0.0.3",
+    version="0.0.4",
     author='Vincenzo Vigilante, Gioele Crispo',
     author_email='info@vvigilante.com',
     package_dir={'ytsubtitles': 'ytsubtitles'},
